c2b985fc43c88f2089bc910efc9f3cf13483fccb
jb <jonasbet@gmail.com>
2021-04-28 14:36:11 +0100

    beggining with HeraclitusModule.py

--- PythonDev/AristotleModule.py
+++ PythonDev/AristotleModule.py
@@ -1,7 +1,9 @@
 
 import ipaddress
+import uuid
 
-
+secretHashSeed = '00010203-0405-0607-0809-0a0b0c0d0e0f'
+nextAvailableNat1sourceIp = "10.0.0.1"
 
 
 def userIpInput():
@@ -15,15 +17,26 @@ def userIpInput():
     print("The given ip is " + ip)
     return ip
 
-def request_new_connection(sourceIp, sourcePort, destIp, destPort):
 
+# >>> # make a UUID using an MD5 hash of a namespace UUID and a name
+# >>> uuid.uuid3(uuid.NAMESPACE_DNS, 'python.org')
+# UUID('6fa459ea-ee8a-3ca4-894e-db77e160355e')
+def hash_creator(sourceIp, sourcePort):
+    return uuid.uuid3(uuid.NAMESPACE_DNS, ( sourceIp + secretHashSeed + sourcePort))
 
+def request_new_connection(sourceIp, sourcePort, destIp, destPort, nextAvailableNat1sourceIp):
     print("\nYour data is now saved as  sourceIp = {}, sourcePort = {}, destIp = {}, destPort = {}. \n".format(sourceIp, sourcePort, destIp, destPort))
-
+    AristotleHash = hash_creator(sourceIp, sourcePort)
+    print(AristotleHash)
+    nat1SourceIP = nextAvailableNat1sourceIp
+    nextAvailableNat1sourceIp += nextAvailableNat1sourceIp
+    print("\nYour data is now saved as    nat1SourceIP = {}, nextAvailableNat1sourceIp  = {}. \n".format(  nat1SourceIP , nextAvailableNat1sourceIp))
+    print("\nWe can now pass HeraclitusModule   nat1SourceIP = {}, AristotleHash  = {}. \n".format(  nat1SourceIP , AristotleHash))
 
 
 def menu():
     loop_condition = True
+
     while loop_condition:
         print("\nWelcome to AristotleModule\n")
         print("\nPlease enter a number for what you want to do.\n")
@@ -42,18 +55,18 @@ def menu():
         elif menu_choice == 1:
             print("\n Please provide the sourceIP in binary form octet by octet....")
             sourceIp = userIpInput()
-
-
-            menu()
-
-        elif menu_choice == 2:
-            print("\n Please provide the sourceIP in binary form octet by octet....")
-            sourceIp = userIpInput()
             print("\n Please provide the dstIP in binary form octet by octet....")
             destIp = userIpInput()
             sourcePort = input("\nWhat is source port)\n")
             destPort = input("\nWhat is source port)\n")
             request_new_connection(sourceIp, sourcePort, destIp, destPort)
+            print("\nYour data is now saved as    nextAvailableNat1sourceIp  = {}. \n".format(
+                nextAvailableNat1sourceIp))
+            menu()
+
+        elif menu_choice == 2:
+# using 2 for testing
+            print(hash_creator())
             menu()
         elif menu_choice == 3:
             sourcePort = input("\nWhat is source port)\n")
